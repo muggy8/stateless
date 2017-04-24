2bec82ad6d00d614b2fdae0219d267b63b701087
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-04-24 15:52:16 -0700

    setup basic inheritance structure

--- templater.js
+++ templater.js
@@ -6,21 +6,78 @@
 		}
 		return JSON.stringify(obj) === JSON.stringify({});
 	}
-    
+
     var ajaxGet = function(url, successCallback, failCallback){
         var request = new XMLHttpRequest();
-        
+
         successCallback = successCallback || function (data){/*console.log(data)*/};
 		failCallback = failCallback || function (data){/*console.log(data)*/};
-        
+
         request.onload = function(){
             successCallback(request)
         };
         request.onerror = function(){
             failCallback(request)
         }
-        
+
         request.open('GET', url, true);
         request.send();
+    }
+
+    var domChunk = function(ele){
+        if (!ele){
+            throw "No element selected";
+        }
+
+        // detect if new or just calling it
+        var self = (context == this)? {} : this;
+
+        // initialize some private values
+        var segment = ele;
+        var insertAt = segment.querySelector("ins") || ele;
+        var children = [];
+        var parent = [];
+
+        // set public methods into the prototype of this object.
+        var public_method = {};
+        Object.setPrototypeOf(self, public_method);
+
+        public_method.on = function(){
+
+        }
+
+        public_method.off = function(){
+
+        }
+
+        public_method.once = function(){
+
+        }
+
+        public_method.appendChild = function(){
+
+        }
+
+        public_method.unlink = function(){
+
+        }
+
+        return self;
+    }
+
+    context.instantiateableChunk = function(ele){
+        // detect if new or just calling it
+        var self = (context == this)? {} : this;
+
+        // simulate "class instantiateableChunk extends domChunk"
+        var actualChunk = new domChunk(ele);
+        Object.setPrototypeOf(self, actualChunk);
+
+        // limit the instantiate function here
+        actualChunk.instantiate = function(){
+
+        }
+
+        return self;
     }
 })(this)
