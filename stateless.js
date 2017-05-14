0d79cd22383ebc384c342f33f171c12a8359f295
muggy8 <muggy8@gmail.com>
2017-05-13 17:19:34 -0700

    adding a bunch of polyfills

--- stateless.js
+++ stateless.js
@@ -1,3 +1,161 @@
+// ---------------------------------
+// polyfills
+// ---------------------------------
+
+// polyfill for bind from MDN
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function(oThis) {
+		if (typeof this !== 'function') {
+			// closest thing possible to the ECMAScript 5
+			// internal IsCallable function
+			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable')
+		}
+
+		var aArgs   = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
+			fNOP    = function() {},
+			fBound  = function() {
+				return fToBind.apply(this instanceof fNOP
+					? this
+					: oThis,
+					aArgs.concat(Array.prototype.slice.call(arguments)))
+			}
+
+		if (this.prototype) {
+			// Function.prototype doesn't have a prototype property
+			fNOP.prototype = this.prototype
+		}
+		fBound.prototype = new fNOP()
+
+		return fBound
+	}
+}
+
+// polyfill for setPrototypeOf from npm
+if (!Object.setPrototypeOf){
+
+	var setProtoOf = function(obj, proto){
+		obj.__proto__ = proto
+		return obj
+	}
+
+	var mixinProperties = function(obj, proto){
+		for (var prop in proto) {
+			if (!obj.hasOwnProperty(prop)) {
+				obj[prop] = proto[prop]
+			}
+		}
+		return obj
+	}
+
+	Object.setPrototypeOf = ({__proto__:[]} instanceof Array ? setProtoOf : mixinProperties)
+}
+
+// polyfill for defineProperty from MDN
+if (!Object.defineProperty){
+	Object.defineProperty = function (obj, properties) {
+		var convertToDescriptor = function (desc) {
+			var hasProperty = function (obj, prop) {
+				return Object.prototype.hasOwnProperty.call(obj, prop)
+			}
+
+			var isCallable = function (v) {
+				// NB: modify as necessary if other values than functions are callable.
+				return typeof v === 'function'
+			}
+
+			if (typeof desc !== 'object' || desc === null){
+				throw new TypeError('bad desc')
+			}
+			var d = {}
+			if (hasProperty(desc, 'enumerable')) d.enumerable = !!desc.enumerable
+			if (hasProperty(desc, 'configurable')) d.configurable = !!desc.configurable
+			if (hasProperty(desc, 'value')) d.value = desc.value
+			if (hasProperty(desc, 'writable')) d.writable = !!desc.writable
+			if (hasProperty(desc, 'get')) {
+				var g = desc.get
+
+				if (!isCallable(g) && typeof g !== 'undefined'){
+					throw new TypeError('bad get')
+				}
+				d.get = g
+			}
+			if (hasProperty(desc, 'set')) {
+				var s = desc.set
+				if (!isCallable(s) && typeof s !== 'undefined') {
+					throw new TypeError('bad set')
+				}
+				d.set = s
+			}
+
+			if (('get' in d || 'set' in d) && ('value' in d || 'writable' in d)) {
+				throw new TypeError('identity-confused descriptor')
+			}
+
+			return d
+		}
+
+		if (typeof obj !== 'object' || obj === null){throw new TypeError('bad obj')}
+
+		properties = Object(properties)
+
+		var keys = Object.keys(properties)
+		var descs = []
+
+		for (var i = 0; i < keys.length; i++){
+			descs.push([keys[i], convertToDescriptor(properties[keys[i]])])
+		}
+
+		for (var i = 0; i < descs.length; i++){
+			Object.defineProperty(obj, descs[i][0], descs[i][1])
+		}
+		return obj
+	}
+}
+
+// Object.keys polyfill
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+	Object.keys = (function() {
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+		hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+		dontEnums = [
+			'toString',
+			'toLocaleString',
+			'valueOf',
+			'hasOwnProperty',
+			'isPrototypeOf',
+			'propertyIsEnumerable',
+			'constructor'
+		],
+		dontEnumsLength = dontEnums.length
+
+		return function(obj) {
+			if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object')
+			}
+
+			var result = [], prop, i
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop)
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i])
+					}
+				}
+			}
+			return result
+		}
+	}())
+}
+
+// actual functions
 (function(context, eval){
 
 	// --------------------------------------------------------
