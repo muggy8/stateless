43b224d36a79d754a2da74ccfd886eced52897d3
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-06-16 13:50:22 -0700

    testing new gulp task thing

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-const gulp = require("gulp")
+const gulp = require("gulp"),
+    uglify = require('gulp-uglify')
 
 gulp.task("default", function(){
     console.log("testing testing 123")
+
+    return gulp.src("stateless.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("stateless.mini.js"))
 })
